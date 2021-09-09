1f7a690468f2bf08f9647a3f85a1fe8d90358ac0
Ulysse <ulysse.raillon@hotmail.fr>
2021-09-10 00:21:23 +0200

    [LD/FEAT] up bulletprefab + bullets rotate

--- GFMW - 2021/Assets/Scripts/Bullet.cs
+++ GFMW - 2021/Assets/Scripts/Bullet.cs
@@ -7,18 +7,25 @@ using Object = System.Object;
 public class Bullet : MonoBehaviour
 {
     public float bulletSpeed = 5f;
+    public SoundEffect sfx;
 
     void Awake()
     {
+        sfx.InitSource(gameObject);
         Destroy(gameObject, 6f);
     }
 
     private void Update()
     {
-        if(GameManager.instance.state == State.INGAME)
+        if (GameManager.instance.state == State.INGAME)
+        {
             transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.back * bulletSpeed/10);
+        }
         else
             Destroy(gameObject);
+     
+        sfx.UpdatePitchToTimeScale();
     }
 
     private void OnTriggerEnter(Collider other)
